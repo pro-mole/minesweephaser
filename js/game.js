54ab144702493692cc8bc2b685fbee5910328100
brazmogu <guedesav@brazmogu.com.br>
2015-12-02 14:51:26 +0000

    Gameplay improved Multiple states implemented

--- js/game.js
+++ js/game.js
@@ -1,14 +1,35 @@
-var game = new Phaser.Game(600, 600, Phaser.AUTO, "sweeper", {
+var menuState = {
+    preload: function() {
+        
+    },
+    
+    create: function() {
+        
+    },
+    
+    update: function() {
+        if (game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR))
+        {
+            game.state.start('gameplay', true);
+        }
+    }
+};
+
+var gameplayState =  {
     preload: function() {
         game.load.spritesheet('mine', 'assets/minesweeper.png',16,16);
         game.load.spritesheet('tile', 'assets/tile.png',16,16);
     },
     
     create: function() {
-        game.grid = new MineGrid(8,8,10);
+        game.grid = new MineGrid(16,16,32);
     },
     
     update: function() {
         game.grid.update();
     }
-});
+};
+
+var game = new Phaser.Game(600, 600, Phaser.AUTO, "sweeper");
+game.state.add('menu', menuState, true);
+game.state.add('gameplay', gameplayState);
--- js/grid.js
+++ js/grid.js
@@ -9,7 +9,9 @@ function MineGrid(width, height, mines) {
     this.tile = [];
     
     this.mine_size = 16;
-    this.graphics = game.add.graphics((game.world.width - this.w*this.mine_size)/2, (game.world.height - this.h*this.mine_size)/2);
+    this.graphics = game.add.graphics((game.world.width - this.w*(this.mine_size+2))/2, (game.world.height - this.h*(this.mine_size+2))/2);
+    
+    this.ended = false;
     
     for(var x=0; x < width; x++)
     {
@@ -31,7 +33,15 @@ function MineGrid(width, height, mines) {
             i--;
             continue;
         }
-        else this.tile[randX][randY].setContents('*');
+        
+        var mine = this.tile[randX][randY];
+        mine.setContents('*');
+        this.mines.push(mine);
+        var n = mine.neighbors();
+        for (var j in n)
+        {
+            n[j].increment();
+        }
     }
     
     this.update = function() {
@@ -42,10 +52,20 @@ function MineGrid(width, height, mines) {
                 this.tile[x][y].update();
             }
         }
+        
+        if (this.ended && game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR))
+        {
+            game.state.restart(true);
+        }
     };
     
     this.gameOver = function() {
         alert("Game Over!");
+        this.ended = true;
+        for(var m in this.mines)
+        {
+            this.mines[m].hidden = false;
+        }
     };
 }
 
@@ -57,29 +77,78 @@ function MineSlot(grid, x, y, contents) {
     this.contents = contents;
     this.hidden = true;
     
-    this.graphics = game.add.graphics(x*this.grid.mine_size, y*this.grid.mine_size);
-    this.tileSprite = game.add.sprite(0, 0, 'tile');
+    this.graphics = game.add.graphics(x*(this.grid.mine_size+2), y*(this.grid.mine_size+2));
+    this.tileSprite = game.add.sprite(1, 1, 'tile');
     this.tileSprite.inputEnabled = true;
     
     this.graphics.addChild(this.tileSprite);
     this.grid.graphics.addChild(this.graphics);
     
-    this.tileSprite.events.onInputDown.add(function() {
-        if (this.hidden)
+    this.reveal = function()
+    {
+        if (!this.grid.ended && this.hidden)
         {
             this.hidden = false;
             if (this.contents == '*') this.grid.gameOver();
+            if (this.contents == 0)
+            {
+                var N = this.neighbors();
+                for (var i in N)
+                {
+                    N[i].reveal();
+                }
+            }
         }
-    }, this);
+    };
+    this.tileSprite.events.onInputDown.add(this.reveal, this);
+    
+    this.neighbors = function()
+    {
+        var neighbors = [];
+        for (var dx=-1; dx<=1; dx++)
+        {
+            for (var dy=-1; dy<=1; dy++)
+            {
+                if (this.x+dx < 0 || this.x+dx >= this.grid.w || this.y+dy < 0 || this.y+dy >= this.grid.h || (dx == 0 && dy == 0)) continue;
+                neighbors.push(this.grid.tile[this.x+dx][this.y+dy]);
+            }
+        }
+        return neighbors;
+    }
     
     this.setContents = function(contents)
     {
-        if (contents == '*')
-            this.contentSprite = game.add.sprite(0, 0, 'mine', 0);
-        else if (contents > 0)
-            this.contentSprite = game.add.sprite(0, 0, 'mine', contents);
-        this.contentSprite.moveDown();
-        this.graphics.addChild(this.contentSprite);
+        this.contents = contents;
+        if (this.contentSprite == undefined)
+        {
+            if (contents == '*')
+                this.contentSprite = game.add.sprite(1, 1, 'mine', 0);
+            else if (contents > 0)
+                this.contentSprite = game.add.sprite(1, 1, 'mine', contents);
+                
+            this.contentSprite.moveDown();
+            this.graphics.addChild(this.contentSprite);
+        }
+        else
+        {
+            if (contents == '*')
+                this.contentSprite.frame = 0;
+            else if (contents > 0)
+                this.contentSprite.frame = contents;
+        }
+    }
+    
+    this.increment = function(delta)
+    {
+        if (delta == undefined) delta = 1;
+        if (this.contents == '*')
+        {
+            console.warn("Incrementing content value of a non-numeric tile at " + this.x + ":" + this.y);
+            return;
+        }
+        
+        this.contents = this.contents+delta;
+        this.setContents(this.contents);
     }
     
     this.update = function()
